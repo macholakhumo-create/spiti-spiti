c08fa31b50fd92acff9ff4402c4394518eac798c
macholakhumo-create <machola.khumo@gmail.com>
2026-04-21 14:11:59 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "logistics",
-  password: "Oloratomachola20",
-  port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
