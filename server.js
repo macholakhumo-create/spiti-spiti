04dad9621fa576e561693322dc842fcf8cfd15e3
macholakhumo-create <machola.khumo@gmail.com>
2026-04-21 13:23:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log("Server running on port " + PORT);
   });
-});
 
 // GET all rides
 app.get("/rides", async (req, res) => {
