d62b2b8d49244f54022b8c4aaaf69bea8458eff2
macholakhumo-create <machola.khumo@gmail.com>
2026-04-23 16:13:26 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
-dotenv.config();
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
-export default pool;
+module.exports = pool;
